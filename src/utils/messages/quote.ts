5b1554a5a2ec5e52150800829557048b300d832b
Jesus Lares <jesus.lares2000@gmail.com>
2022-09-05 05:06:08 -0500

    feat: add routes and controller of quote collection

---
+++ src/utils/messages/quote.ts
@@ -0,0 +1,13 @@
+import { typesObjectString } from "@utils/constants";
+
+const quoteMessages: typesObjectString = {
+  GET__ALL: "Lista de citas cargada correctamente.",
+  DATA_EMPTY: "No se recibieron datos para el registro.",
+  CREATE_SUCCESS: "Cita creada correctamente.",
+  DELETE_SUCCESS: "Cita eliminada correctamente.",
+  UPDATE_SUCCESS: "Cita actualizada correctamente.",
+  ERROR_DEFAULT:
+    "Error al realizar la petición. Comprueba que tienes corretamente todo.",
+};
+
+export default quoteMessages;
